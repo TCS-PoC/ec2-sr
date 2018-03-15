27091688cde37edd24f3869d82123c20f1ec6c3e
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-15 15:28:16 +0530

    Update prov1.tf

--- prov1.tf
+++ prov1.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  access_key = "${access_key}"
-  secret_key = "${secret_key}"
-  region     = "${region}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "instance1" {
