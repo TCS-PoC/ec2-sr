9e7e663582cc823a8534a60e3735b20b910564f6
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-15 17:28:24 +0530

    Update prov2.tf

--- prov2.tf
+++ prov2.tf
@@ -3,6 +3,7 @@ variable "secret_key" {}
 variable "region" {}
 
 provider "aws" {
+  alias  = "second"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
