c16f44ae1ca1ad2eb16ac09096d6e50c87a42d47
shalabh-a <37323672+shalabh-a@users.noreply.github.com>
2018-03-16 17:59:28 +0530

    Create prov3.tf

---
+++ prov3.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  alias  = "second"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "instance2" {
+  ami = "ami-26ebbc5c"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  disable_api_termination = "true"
+  key_name = "key1"
+  security_groups = "sg-74382e1c"
+  subnet_id = "subnet-d69c57be" 
+  tags {
+    Name = "hostTerraform3"
+  }
+}
